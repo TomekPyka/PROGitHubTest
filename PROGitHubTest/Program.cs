c9801643180601f2dc248bd9a48e2e11271c0602
TomekPyka <61656933+TomekPyka@users.noreply.github.com>
2020-10-19 09:55:28 +0200

    Dodaj pliki projektów.

---
+++ PROGitHubTest/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace PROGitHubTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program program = new Program();
+            program.helloWorlds();
+        }
+
+        public void helloWorld()
+        {
+            Console.WriteLine("Hello World from a method!");
+        }
+    }
+}
